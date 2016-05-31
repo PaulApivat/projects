649c5ff4d616bdc46a3703cf560405fd9cdefb0a
Paul Apivat <paul.apivat@gmail.com>
2016-05-31 09:37:12 +0700

    migrate from front page

---
+++ paul.js
@@ -0,0 +1,28 @@
+
+
+$(document).ready(function(){
+
+  $('div.nav').hover(
+    function(){
+    $(this).addClass('active');
+    },
+    function(){
+    $(this).removeClass('active');    
+    }
+  );
+
+});
+
+
+$(document).ready(function(){
+
+  $('div.pro').hover(
+    function(){
+    $(this).addClass('active1');
+    },
+    function(){
+    $(this).removeClass('active1');    
+    }
+  );
+
+});
